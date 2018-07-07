4ae4b15c9a3acd01273154b8031102d6d2a42395
Alexandr <shlyaeu@gmail.com>
2018-07-07 19:45:19 +0300

    auth registration reworked

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,9 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/Users')
+
+
+
+
 module.exports.login = function (req, res) {
   res.status(200).json(
     {
@@ -10,9 +16,32 @@ module.exports.login = function (req, res) {
   
 }
 
-module.exports.register = function (req, res) {
-  res.status(200).json({
-    register: 'from controller'
-  })
-  
+module.exports.register = async function (req, res) {
+  //email + password
+  const candidate = await User.findOne({email: req.body.email})
+  if (candidate){
+    // user is allready exits, trows error
+    res.status(409).json({
+      message: 'Такой email уже занят. Попробуйте другой.'
+    })
+  }
+  else{
+    // create user
+    const salt = bcrypt.genSaltSync(10)
+    const password = req.body.password
+    const user = new User({
+      email: req.body.email,
+      password: bcrypt.hashSync(password, salt)
+    })
+    
+    // save to database + errorhandling
+    try {
+      await user.save()
+      res.status(201).json(user)
+    } 
+    catch(e){
+      //error
+      
+    }
+  }
 }
