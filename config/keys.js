c39a1a8151a74e4e03c20bfb13057dec489da1d3
Alexandr <shlyaeu@gmail.com>
2018-07-07 20:07:04 +0300

    auth fuul complite

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://koji6acbi4:Terran88@ds161610.mlab.com:61610/example_crm_db'
+  mongoURI: 'mongodb://koji6acbi4:Terran88@ds161610.mlab.com:61610/example_crm_db',
+  jwt: 'dev-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,38 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/Users')
+const keys = require('../config/keys')
 
 
 
 
-module.exports.login = function (req, res) {
-  res.status(200).json(
-    {
-      login: {
-        email: req.body.email,
-        password: req.body.password,
-      }
+module.exports.login = async function (req, res) {
+  const candidate = await User.findOne({email: req.body.email})
+  if (candidate){
+    // проверка пороля
+    const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+    if (passwordResult){
+      //генерация токена, пароли совпали
+      const token = jwt.sign({
+        email: candidate.email,
+        userId: candidate._id
+      }, keys.jwt, {expiresIn: 60 * 60})
+
+      res.status(200).json({
+        token: `Bearer ${token}`
+      })
+    }else{
+      //  пароли не совпали
+      res.status(401).json({
+        message: 'Пароли не совпадают. Пробуйте сново'
+      })
     }
-  )
-  
+  } else{
+    //error
+    res.status(404).json({
+      message: 'Пользователь с таким email не найден'
+    })
+  }
 }
 
 module.exports.register = async function (req, res) {
