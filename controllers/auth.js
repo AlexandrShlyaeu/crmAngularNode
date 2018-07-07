a0f843a4d9d7abde306fa3162b34a5b748594413
Alexandr <shlyaeu@gmail.com>
2018-07-07 17:40:49 +0300

    auth controllers (reg + log)

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,3 +6,10 @@ module.exports.login = function (req, res) {
   )
   
 }
+
+module.exports.register = function (req, res) {
+  res.status(200).json({
+    register: 'from controller'
+  })
+  
+}
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const controller = require('../controllers/auth')
 
 // localhost:5000/api/auth/login
 router.get('/login', controller.login)
+// localhost:5000/api/auth/register
+router.get('/register', controller.register)
 
 
 
